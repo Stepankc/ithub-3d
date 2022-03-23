988ef89967b2470a7669a7cb5d718511f6925bc7
KuznetsovR <abcruslan2004@mail.ru>
2022-03-23 16:17:29 +0500

    updated portal timer size and colors

--- src/Threejs/Javascript/World/Portals/PortalTimer.js
+++ src/Threejs/Javascript/World/Portals/PortalTimer.js
@@ -20,13 +20,13 @@ export default class PortalTimer{
     this.visibleTimer._element = document.createElement('div');
     this.visibleTimer._element.style.userSelect = 'none';
     this.visibleTimer._element.style.position = 'fixed';
-    this.visibleTimer._element.style.background = '#ff9933';
-    this.visibleTimer._element.style.borderRadius = '5px';
+    this.visibleTimer._element.style.background = 'linear-gradient(to right, #12c2e9, #c471ed ,#f64f59)';
+    this.visibleTimer._element.style.borderRadius = '15px';
     this.visibleTimer._element.style.border = '1px solid black';
     this.visibleTimer._element.style.top = '50px';
     this.visibleTimer._element.style.left = '50%';
     this.visibleTimer._element.style.transform = 'translate(-50%, 0)';
-    this.visibleTimer._element.style.height = '10px';
+    this.visibleTimer._element.style.height = '15px';
     this.visibleTimer._element.style.transition = '1s';
     this.visibleTimer._element.style.opacity = '0';
     document.body.appendChild(this.visibleTimer._element);
